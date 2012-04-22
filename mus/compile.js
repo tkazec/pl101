cdb6983ac1c0bbc1bd2dd4dbd389d415132a985e
Teddy Cross <tkazec@gmail.com>
2012-04-21 21:45:28 -0400

    Add compiler from part 1 of lesson 2

---
+++ mus/compile.js
@@ -0,0 +1,25 @@
+var compile = function (musexpr, time) {
+	var root = time === undefined,
+		left, right;
+	
+	time = time || 0;
+	
+	if (musexpr.tag === "note") {
+		musexpr.start = time;
+		
+		return (root ? [] : [musexpr.dur]).concat(musexpr);
+	} else if (musexpr.tag === "seq") {
+		left = compile(musexpr.left, time);
+		time += left.shift();
+		
+		right = compile(musexpr.right, time);
+		time += right.shift();
+	} else if (musexpr.tag === "par") {
+		left = compile(musexpr.left, time);
+		right = compile(musexpr.right, time);
+		
+		time += Math.max(left.shift(), right.shift());
+	}
+	
+	return (root ? [] : [time]).concat(left, right);
+};

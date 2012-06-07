94fe55305380900f13c73c9a8cc741b3b85e8ca1
Teddy Cross <tkazec@gmail.com>
2012-06-06 22:04:11 -0400

    Update Scheem tests

--- scheem/test.js
+++ scheem/test.js
@@ -10,25 +10,58 @@ var data = fs.readFileSync(__dirname + "/grammar.peg", "utf-8");
 var parse = PEG.buildParser(data).parse;
 
 
+/*** helpers ***/
+var testGrammar = function (src, out) {
+	assert.deepEqual(parse(src), out);
+};
+
+
 /*** test ***/
-describe("Scheem", function () {
+describe("The grammar", function () {
 	it("should parse atoms", function () {
-		assert.deepEqual(parse("atom"), "atom");
-		assert.deepEqual(parse("+"), "+");
+		testGrammar("atom", "atom");
+		testGrammar("+", "+");
 	});
 	
 	it("should parse expressions", function () {
-		assert.deepEqual(parse("(+ x 3)"), ["+", "x", "3"]);
+		testGrammar(
+			"(+ x 3)",
+			["+", "x", "3"]
+		);
 	});
 	
 	it("should parse nested expressions", function () {
-		assert.deepEqual(parse("(+ 1 (f x 3 y))"), ["+", "1", ["f", "x", "3", "y"]]);
+		testGrammar(
+			"(+ 1 (f x 3 y))",
+			["+", "1", ["f", "x", "3", "y"]]
+		);
 	});
 	
 	it("should parse crazy stuff", function () {
-		assert.deepEqual(
-			parse("(define factorial (lambda (n) (if (= n 0) 1 (* n (factorial (- n 1))))))"),
+		testGrammar(
+			"(define factorial (lambda (n) (if (= n 0) 1 (* n (factorial (- n 1))))))",
 			["define", "factorial", ["lambda", ["n"], ["if", ["=", "n", "0"], "1", ["*", "n", ["factorial", ["-", "n", "1"]]]]]]
+		);
+	});
+	
+	it("should allow any whitespace everywhere", function () {
+		testGrammar(
+			"\n(f (  x ) ( x * x  ) )\t",
+			["f", ["x"], ["x", "*", "x"]]
+		);
+	});
+	
+	it("should ignore comments", function () {
+		testGrammar(
+			"(+;;test\n1 1)",
+			["+", "1", "1"]
+		);
+	});
+	
+	it("should ease quoting", function () {
+		testGrammar(
+			"(a 'b '(c d) e)",
+			["a", ["quote", "b"], ["quote", ["c", "d"]], "e"]
 		);
 	});
 });
